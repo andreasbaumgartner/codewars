9ae87f2e6b1ba4d54ba34e85d46490ae12056bc0
andreasbaumgartner <baumgartner.andy@hotmail.ch>
2023-04-24 23:59:33 +0200

    update python ip checker

--- ip_address_checker.py
+++ ip_address_checker.py
@@ -1,12 +1,11 @@
-
 strng = "123.122.12.23"
 
 
 def is_valid_IP(strng):
-    if strng.count('.') != 3:
+    if strng.count(".") != 3:
         return False
-    for i in strng.split('.'):
-        if not i.isdigit() or (i[0] == '0' and len(i) > 1) or int(i) > 255:
+    for i in strng.split("."):
+        if not i.isdigit() or (i[0] == "0" and len(i) > 1) or int(i) > 255:
             return False
     return True
 
